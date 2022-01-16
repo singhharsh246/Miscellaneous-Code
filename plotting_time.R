0d54eb0258b5cbb845d37b92e1530ffca5ffd948
Harsh <singhharsh246@gmail.com>
2022-01-16 09:33:00 -0800

    Create plotting_time.R
    time series analysis part 1. Find the related article here: https://singhharsh246.medium.com/time-series-analysis-part-1-2e05af6569b

---
+++ plotting_time.R
@@ -0,0 +1,87 @@
+setwd('/Users/rctrj/Downloads/time_series')
+
+install.packages('ggfortify')
+install.packages('fpp2')
+install.packages('GGally')
+install.packages('ts_plot')
+
+library(ts_plot)
+library(ggplot2)
+library(ggfortify)
+library(fpp2)
+library(forecast)
+library(GGally)
+
+
+
+#### reading the file ####
+
+data <- read.csv('DailyDelhiClimateTrain.csv')
+df <- ts(data, start=c(2013,1,1), frequency=365)
+
+data1 <- read.csv('DailyDelhiClimateTest.csv')
+df1 <- ts(data1, start=c(2017,1,1), frequency=365)
+
+
+colnames(df)
+
+autoplot(df[,"humidity"]) +
+  ggtitle("Humidity") + xlab("Year") +
+  ylab("Humidity")
+
+colnames(df)
+
+
+
+colnames(df)
+autoplot(df[,"humidity"], xlab = "Year", ylab = "Number of Newly Joined Members")
+
+
+###### decomposing data into trend and season #######
+
+fit <- tslm(humidity ~ trend + season, data = df)
+summary(fit)
+
+#### plot this #####
+df %>% as.data.frame() %>% GGally::ggpairs()
+
+
+#### naive, seasonal naive, mean method
+autoplot(df[,'humidity']) +
+  autolayer(meanf(df[,'humidity'], h=365), series="Mean", PI=FALSE) +
+  autolayer(naive(df[,'humidity'], h=365), series="Naïve", PI=FALSE) +
+  autolayer(rwf(df[,'humidity'], drift=TRUE, h=365), series="Drift", PI=FALSE) +
+  autolayer(snaive(df[,'humidity'], h=365), series="Seasonal naïve", PI=FALSE) +
+  autolayer(df1[,'humidity'], series="Actual", PI=FALSE) +
+  ggtitle("Comparison of actual humidity with humidity forecast") + xlab("Year") + 
+  ylab("Humidity") + guides(colour=guide_legend(title="Forecast"))
+
+
+
+#### bias adjusted and back-transformed ####
+
+fc <- rwf(df[,'humidity'], drift=TRUE, lambda=0, h=50, level=80) 
+fc2 <- rwf(df[,'humidity'], drift=TRUE, lambda=0, h=50, level=80,
+           biasadj=TRUE) 
+autoplot(df[,'humidity']) +
+  autolayer(fc, series="Simple back transformation") + 
+  autolayer(fc2, series="Bias adjusted", PI=FALSE) + 
+  guides(colour=guide_legend(title="Forecast"))
+
+#### season and trend on tslm ####
+
+ts.model <- tslm(df[,'humidity'] ~ trend + season)
+checkresiduals(ts.model)
+
+forecast(ts.model, h = 365)
+
+plot(forecast(ts.model, h = 365))
+
+autoplot(df[,'humidity']) +
+  autolayer(forecast(ts.model, h = 100), series = "Linear Regression") +
+  autolayer(df1[,'humidity'], series="Actual", PI=FALSE) +
+  ggtitle("Comparison of actual humidity with humidity forecast") + xlab("Year") + 
+  ylab("Megalitres") + guides(colour=guide_legend(title="Forecast"))
+
+
+checkresiduals(ts.model)
